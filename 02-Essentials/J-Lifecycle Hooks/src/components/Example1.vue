47dbd7bee381b57428525941bfcba0689fb07350
mangwu <1185956753@qq.com>
2023-06-22 06:19:07 +0800

    Lifecycle Hooks

---
+++ 02-Essentials/J-Lifecycle Hooks/src/components/Example1.vue
@@ -0,0 +1,21 @@
+<script setup lang="js">
+import {onMounted,ref} from "vue";
+const currentTime = Date.now();
+const message = ref("")
+onMounted(() => {
+  console.log("the component is now mounted.");
+  message.value = Date.now() - currentTime
+})
+</script>
+
+<template>
+  <div class="example example1">
+    Example1 component initial cost {{ message }} ms.
+  </div>
+</template>
+
+<style scoped lang="css">
+.example1:hover::before {
+  content: "1. Registering Lifecycle Hooks";
+}
+</style>
